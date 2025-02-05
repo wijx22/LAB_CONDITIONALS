c202ff80d2bc8b134c12b3846f3ff0db8deeb5a3
wijdan <wjdanalbdalzyz@gmail.com>
2025-02-05 12:13:17 +0300

    Add rate movie solution

---
+++ condition.py
@@ -0,0 +1,13 @@
+movie = "squid game"
+rate = 1
+popoularity =72.65
+
+
+if rate >= 4 and popoularity >80:
+    print("highly recommended")
+elif rate >= 3 and popoularity >70:
+    print("I recommended it . it is good")    
+elif rate <= 2 and popoularity >60:
+    print("You should cheak it out !")   
+elif rate <= 2 and popoularity < 50:
+    print ("Do not watch it . it is a waste of time")
